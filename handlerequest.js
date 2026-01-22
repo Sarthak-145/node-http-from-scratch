0180fd08964816f8b8d6a9995aed010d71b5bd41
Sarthak-145 <sarthakkanawade145@gmail.com>
2026-01-22 22:14:36 +0530

    close the connection when client says close in header

--- handlerequest.js
+++ handlerequest.js
@@ -1,16 +1,26 @@
-const handleRequest = (socket, reqest, body) => {
-  console.log('request obj: ', reqest);
-  console.log('body in text: ', body.toString('utf-8'));
+const handleRequest = (socket, request, body) => {
+  //console.log('request obj: ', request);
+  //console.log('body in text: ', body.toString('utf-8'));
 
-  const resBody = `Hello client I got ${body.length} bytes of body from you :)`;
-  const res =
-    `HTTP/1.1 200 OK\r\n` +
-    `Content-Length: ${Buffer.byteLength(resBody)}\r\n` +
-    `Content-Type: text/plain\r\n` +
-    `Connection: keep-alive\r\n` +
-    `\r\n` +
-    resBody;
-  socket.write(res);
+  if (request.headers.connection === 'close') {
+    socket.end(
+      `HTTP/1.1 200 OK\r\n` +
+        `Content-Length: 0\r\n` +
+        `Content-Type: text/plain\r\n` +
+        `Connection: close\r\n` +
+        `\r\n`
+    );
+  } else {
+    const resBody = `Hello client I got ${body.length} bytes of body from you :)`;
+    const res =
+      `HTTP/1.1 200 OK\r\n` +
+      `Content-Length: ${Buffer.byteLength(resBody)}\r\n` +
+      `Content-Type: text/plain\r\n` +
+      `Connection: keep-alive\r\n` +
+      `\r\n` +
+      resBody;
+    socket.write(res);
+  }
 };
 
 export default handleRequest;
