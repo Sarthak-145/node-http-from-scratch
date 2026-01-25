7cb25e64efda852796101e37b0fff2e44026c9aa
Sarthak-145 <sarthakkanawade145@gmail.com>
2026-01-25 14:14:22 +0530

    add /auth/login route and just echo the body

--- handlerequest.js
+++ handlerequest.js
@@ -11,6 +11,17 @@ const handleRequest = (socket, request, body) => {
     socket.end();
     return;
   }
+
+  if (request.method === 'POST' && request.path === '/auth/login') {
+    const bodyText = body.toString('utf-8');
+    // here whole controller things will happen
+    // like check db, match password and then sign a token.
+
+    // for now just a normal message
+    send(socket, bodyText);
+    return;
+  }
+
   send(socket, `Hello client I got ${body.length} bytes of body from you :)`);
   console.log('request is responded!');
 };
