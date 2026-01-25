ccd5169d315794118c3b7306bd34204c6301fb2c
Sarthak-145 <sarthakkanawade145@gmail.com>
2026-01-25 12:49:06 +0530

    default header and 'write' automatation

---
+++ response.js
@@ -0,0 +1,20 @@
+const send = (socket, body = '', status = 200, headers = {}) => {
+  const defaultHeaders = {
+    'Content-Type': 'text/plain',
+    'Content-Length': Buffer.byteLength(body),
+    Connection: 'keep-alive',
+  };
+
+  const allHeaders = { ...defaultHeaders, ...headers };
+
+  const headerStr = `HTTP/1.1 ${status} OK\r\n`;
+
+  // we can say anti-parsing. we want to send string and not an object.
+  for (const key in allHeaders) {
+    headerStr += `${key}:${allHeaders[key]}\r\n`;
+  }
+
+  socket.write(headerStr, '\r\n', body);
+};
+
+export default send;
